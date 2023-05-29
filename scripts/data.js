858a26291dfca3d9dbd39f1587d0bd27989f365c
addoxy <aditya132003@gmail.com>
2023-05-29 14:22:13 +0530

    Add box shadow to begin button and add quiz page

---
+++ scripts/data.js
@@ -0,0 +1,105 @@
+const data = [
+  {
+    question: 'Which planet is known as the "Red Planet"?',
+    options: {
+      a: "Mercury",
+      b: "Mars",
+      c: "Venus",
+      d: "Jupiter",
+    },
+    answer: b,
+  },
+  {
+    question: 'Who painted the famous artwork "Mona Lisa"?',
+    options: {
+      a: "Vincent van Gogh",
+      b: "Pablo Picasso",
+      c: "Leonardo da Vinci",
+      d: "Michelangelo",
+    },
+    answer: c,
+  },
+  {
+    question: "What is the capital city of Australia?",
+    options: {
+      a: "Sydney",
+      b: "Canberra",
+      c: "Melbourne",
+      d: "Brisbane",
+    },
+    answer: b,
+  },
+  {
+    question: 'Which country is known as the "Land of the Rising Sun"?',
+    options: {
+      a: "China",
+      b: "South Korea",
+      c: "Japan",
+      d: "Vietnam",
+    },
+    answer: c,
+  },
+  {
+    question: 'Who wrote the play "Romeo and Juliet"?',
+    options: {
+      a: "William Shakespeare",
+      b: "Charles Dickens",
+      c: "Jane Austen",
+      d: "Mark Twain",
+    },
+    answer: a,
+  },
+  {
+    question: "Which country is famous for the ancient pyramids?",
+    options: {
+      a: "Greece",
+      b: "Egypt",
+      c: "Mexico",
+      d: "Italy",
+    },
+    answer: b,
+  },
+  {
+    question: "Which animal is known for its black and white stripes?",
+    options: {
+      a: "Giraffe",
+      b: "Panda",
+      c: "Zebra",
+      d: "Cheetah",
+    },
+    answer: c,
+  },
+  {
+    question:
+      "Who is credited with discovering gravity after seeing an apple fall from a tree?",
+    options: {
+      a: "Isaac Newton",
+      b: "Albert Einstein",
+      c: "Galileo Galilei",
+      d: "Thomas Edison",
+    },
+    answer: a,
+  },
+  {
+    question: "What is the largest ocean on Earth?",
+    options: {
+      a: "Atlantic Ocean",
+      b: "Indian Ocean",
+      c: "Arctic Ocean",
+      d: "Pacific Ocean",
+    },
+    answer: d,
+  },
+  {
+    question: "Which famous scientist developed the theory of relativity?",
+    options: {
+      a: "Isaac Newton",
+      b: "Marie Curie",
+      c: "Albert Einstein",
+      d: "Nikola Tesla",
+    },
+    answer: c,
+  },
+];
+
+export default data;
