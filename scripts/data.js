060866df46300b4bd56dc0ef859b0b9f77cb13a7
addoxy <aditya132003@gmail.com>
2023-05-30 12:30:48 +0530

    Remove quiz.html and put the quiz section in index.html with hidden class

--- scripts/data.js
+++ scripts/data.js
@@ -1,103 +1,73 @@
 const data = [
   {
     question: 'Which planet is known as the "Red Planet"?',
-    options: {
-      a: "Mercury",
-      b: "Mars",
-      c: "Venus",
-      d: "Jupiter",
-    },
+    options: ["Mercury", "Mars", "Venus", "Jupiter"],
     answer: "Mars",
   },
   {
     question: 'Who painted the famous artwork "Mona Lisa"?',
-    options: {
-      a: "Vincent van Gogh",
-      b: "Pablo Picasso",
-      c: "Leonardo da Vinci",
-      d: "Michelangelo",
-    },
+    options: [
+      "Vincent van Gogh",
+      "Pablo Picasso",
+      "Leonardo da Vinci",
+      "Michelangelo",
+    ],
     answer: "Leonardo da Vinci",
   },
   {
     question: "What is the capital city of Australia?",
-    options: {
-      a: "Sydney",
-      b: "Canberra",
-      c: "Melbourne",
-      d: "Brisbane",
-    },
+    options: ["Sydney", "Canberra", "Melbourne", "Brisbane"],
     answer: "Canberra",
   },
   {
     question: 'Which country is known as the "Land of the Rising Sun"?',
-    options: {
-      a: "China",
-      b: "South Korea",
-      c: "Japan",
-      d: "Vietnam",
-    },
+    options: ["China", "South Korea", "Japan", "Vietnam"],
     answer: "Japan",
   },
   {
     question: 'Who wrote the play "Romeo and Juliet"?',
-    options: {
-      a: "William Shakespeare",
-      b: "Charles Dickens",
-      c: "Jane Austen",
-      d: "Mark Twain",
-    },
+    options: [
+      "William Shakespeare",
+      "Charles Dickens",
+      "Jane Austen",
+      "Mark Twain",
+    ],
     answer: "William Shakespeare",
   },
   {
     question: "Which country is famous for the ancient pyramids?",
-    options: {
-      a: "Greece",
-      b: "Egypt",
-      c: "Mexico",
-      d: "Italy",
-    },
+    options: ["Greece", "Egypt", "Mexico", "Italy"],
     answer: "Egypt",
   },
   {
     question: "Which animal is known for its black and white stripes?",
-    options: {
-      a: "Giraffe",
-      b: "Panda",
-      c: "Zebra",
-      d: "Cheetah",
-    },
+    options: ["Giraffe", "Panda", "Zebra", "Cheetah"],
     answer: "Zebra",
   },
   {
     question:
       "Who is credited with discovering gravity after seeing an apple fall from a tree?",
-    options: {
-      a: "Isaac Newton",
-      b: "Albert Einstein",
-      c: "Galileo Galilei",
-      d: "Thomas Edison",
-    },
+    options: [
+      "Isaac Newton",
+      "Albert Einstein",
+      "Galileo Galilei",
+      "Thomas Edison",
+    ],
     answer: "Isaac Newton",
   },
   {
     question: "What is the largest ocean on Earth?",
-    options: {
-      a: "Atlantic Ocean",
-      b: "Indian Ocean",
-      c: "Arctic Ocean",
-      d: "Pacific Ocean",
-    },
+    options: [
+      "Atlantic Ocean",
+      "Indian Ocean",
+      "Arctic Ocean",
+      "Pacific Ocean",
+    ],
     answer: "Pacific Ocean",
   },
   {
     question: "Which famous scientist developed the theory of relativity?",
-    options: {
-      a: "Isaac Newton",
-      b: "Marie Curie",
-      c: "Albert Einstein",
-      d: "Nikola Tesla",
-    },
+    options: ["Isaac Newton", "Marie Curie", "Albert Einstein", "Nikola Tesla"],
     answer: "Albert Einstein",
   },
 ];
