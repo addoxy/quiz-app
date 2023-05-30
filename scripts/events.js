8a97d6f067a0563e61a2aa570b30543ea5b41f7e
addoxy <aditya132003@gmail.com>
2023-05-30 12:40:30 +0530

    Get question and options, create questionIndex and correctAnswers counter, define functions to get enxt question, shuffle questions, run quiz and check answer

---
+++ scripts/events.js
@@ -0,0 +1,16 @@
+const question = document.querySelector("#question");
+
+const options = {
+  option1: document.querySelector("#option1"),
+  option2: document.querySelector("#option2"),
+  option3: document.querySelector("#option3"),
+  option4: document.querySelector("#option4"),
+};
+
+let questsionIndex = 0;
+let correctAnswers = 0;
+
+function nextQuestion() {}
+function shuffleQuestions() {}
+function runQuiz() {}
+function checkAnswer() {}
