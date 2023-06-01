d4d89345e089167c6df6b24f449b329dc13d5902
addoxy <aditya132003@gmail.com>
2023-06-01 13:19:57 +0530

    Add modal to save screen

--- scripts/end.js
+++ scripts/end.js
@@ -1,3 +1,21 @@
 const finalScore = document.querySelector("#final-score");
+const nameInput = document.querySelector("#name-input");
+const saveBtn = document.querySelector("#save-btn");
+const modal = document.querySelector("#modal");
+const closeModal = document.querySelector("#close-modal");
 
 finalScore.innerText = localStorage.getItem("latest-score");
+
+saveBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (nameInput.value === "") {
+    modal.showModal();
+  } else {
+    // append the score to the highscores list
+  }
+});
+
+closeModal.addEventListener("click", (e) => {
+  e.preventDefault();
+  modal.close();
+});
